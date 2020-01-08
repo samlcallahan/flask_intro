aac97e7823cc27af3e5de5439ec4faacc8ae30d2
Sam Callahan <callahan.l.sam@gmail.com>
2020-01-08 09:19:37 -0600

    Elaborated on flask site. Added spam/ham detector and greeting

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import numpy as np
+from model import predict
 
 app = Flask(__name__)
 
@@ -13,3 +14,22 @@ def dice(n_dice):
     for _ in range(int(n_dice)):
         rolls.append(str(np.random.randint(1,7)))
     return render_template('dice.html', n_dice=n_dice, rolls=rolls)
+
+@app.route('/greeting')
+def show_greeting_form():
+    return render_template('greeting.html')
+
+@app.route('/greeting-result', methods=["POST"])
+def show_gretting_result():
+    name = request.form['name']
+    return render_template('greeting-result.html', name=name)
+
+@app.route('/spam-or-ham')
+def show_spam_form():
+    return render_template('spam-or-ham.html')
+
+@app.route('/spam-or-ham-result', methods=["POST"])
+def show_spam_result():
+    message = request.form['message']
+    result = predict(message)
+    return render_template('spam-or-ham-result.html', result=result)
